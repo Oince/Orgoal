15369e6bdbe8a605c26ba9a0d7baf7d1c64e512a
mozziDog <45249430+MozziDog@users.noreply.github.com>
2022-05-30 14:57:20 +0900

    Update NavBar.vue

--- orgoal/src/front/src/components/NavBar.vue
+++ orgoal/src/front/src/components/NavBar.vue
@@ -8,8 +8,14 @@
         <div class="navbar-menu">
           <div class="navbar-left">
             <div class="search">
-              <input v-model="searchText" @keyup.enter="onClickSearchButton" />
-              <button @click="onClickSearchButton">검색</button>
+              <input
+                class="search-input"
+                v-model="searchText"
+                @keyup.enter="onClickSearchButton"
+              />
+              <button class="search-button" @click="onClickSearchButton">
+                검색
+              </button>
             </div>
           </div>
           <div class="navbar-right">
@@ -21,7 +27,20 @@
             <div v-if="isSignedin">
               <span>{{ username }}님 </span>
               <router-link to="/mypage">
-                <button class="mypage">MY</button>
+                <button class="mypage">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    class="bi bi-person"
+                    viewBox="0 0 16 16"
+                  >
+                    <path
+                      d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"
+                    />
+                  </svg>
+                </button>
               </router-link>
             </div>
           </div>
@@ -41,7 +60,8 @@ export default {
   setup: function () {
     // data
     const store = useStore(); // 훅을 사용하여 vuex store 호출
-    const isSignedin = computed(() => store.getters["signin/hasToken"]);
+    let isSignedin = computed(() => store.getters["signin/hasToken"]);
+    isSignedin = true;
     let searchText = "";
 
     //methods
@@ -82,13 +102,16 @@ export default {
   display: flex;
   justify-content: space-between;
 }
+.search {
+  height: 40px;
+}
 .mypage {
   color: gray;
   background-color: white;
   font-weight: 900;
   font-size: 14pt;
-  width: 45px;
-  height: 45px;
+  width: 40px;
+  height: 40px;
   border-radius: 50%;
   border: 1px solid #dfdfdf;
 }
