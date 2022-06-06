36219e80769d6df07c6997412197a3475aa0cd6e
김태현 <th2252@gmail.com>
2022-06-06 21:39:13 +0900

    액티비티 참가신청 기능 추가

--- orgoal/src/front/src/components/ActivityDetail.vue
+++ orgoal/src/front/src/components/ActivityDetail.vue
@@ -12,7 +12,12 @@
           >
         </div>
         <div>
-          <input type="button" class="activity-apply-button" value="참가신청" />
+          <input
+            type="button"
+            class="activity-apply-button"
+            value="참가신청"
+            @click="onClickApplyButton"
+          />
         </div>
       </div>
     </div>
@@ -24,6 +29,26 @@
       </div>
     </div>
   </div>
+  <div v-if="showApplyForm" class="activity-apply-form-container">
+    <div class="activity-apply-form">
+      <p>
+        <label for="commentInput">코멘트</label>
+        <textarea
+          type="text"
+          id="commentInput"
+          class="input_text"
+          ref="emailInput"
+          v-model="applyComment"
+          placeholder="코멘트를 입력하세요"
+          rows="6"
+        />
+      </p>
+      <p class="buttons">
+        <button @click.prevent="doApply()" class="button blue">신청</button>
+        <button @click.prevent="doCancel()" class="button">취소</button>
+      </p>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -32,10 +57,12 @@ import { useRoute } from "vue-router";
 export default {
   name: "ActivityList",
   setup: function () {
+    const router = useRoute();
+    const axios = require("axios").default;
     // data
-    var activityID = "123";
+    let activityID = "123";
     let activityTitle = ref("동작구 상도동 모각코 그룹 모집"); // 레이아웃 디자인용 placeholder
-    var activityContents = ref([
+    let activityContents = ref([
       "대통령이 궐위되거나 사고로 인하여 직무를 수행할 수 없을 때에는 국무총리, 법률이 정한 국무위원의 순서로 그 권한을 대행한다. 모든 국민은 거주·이전의 자유를 가진다. 대법원과 각급법원의 조직은 법률로 정한다. 감사원은 원장을 포함한 5인 이상 11인 이하의 감사위원으로 구성한다. 제안된 헌법개정안은 대통령이 20일 이상의 기간 이를 공고하여야 한다.",
       "국정의 중요한 사항에 관한 대통령의 자문에 응하기 위하여 국가원로로 구성되는 국가원로자문회의를 둘 수 있다. 이 헌법은 1988년 2월 25일부터 시행한다. 다만, 이 헌법을 시행하기 위하여 필요한 법률의 제정·개정과 이 헌법에 의한 대통령 및 국회의원의 선거 기타 이 헌법시행에 관한 준비는 이 헌법시행 전에 할 수 있다.",
       "대한민국의 국민이 되는 요건은 법률로 정한다. 외국인은 국제법과 조약이 정하는 바에 의하여 그 지위가 보장된다. 헌법재판소는 법률에 저촉되지 아니하는 범위안에서 심판에 관한 절차, 내부규율과 사무처리에 관한 규칙을 제정할 수 있다. 법률은 특별한 규정이 없는 한 공포한 날로부터 20일을 경과함으로써 효력을 발생한다.",
@@ -44,13 +71,32 @@ export default {
     var activityHostEmail = ref("admin@orgoal.com");
     var activityHostNickname = ref("김오르골");
     var activityLastModificationDate = ref("2022-05-18");
+
+    let showApplyForm = ref(false);
+    let applyComment = ref("");
+
     // methods
+    let onClickApplyButton = function () {
+      showApplyForm.value = true;
+      console.log("참가신청 버튼");
+    };
+    let doApply = function () {
+      const URI = `/api/activity/${activityID}`;
+      let applyInfo = {
+        answer: applyComment.value,
+      };
+      let config = {
+        headers: {
+          token: this.token,
+        },
+      };
+      axios.post(URI, applyInfo, config);
+    };
 
     // lifecycle hook
     onMounted(() => {
-      const router = useRoute();
       activityID = router.params.id;
-      const axios = require("axios").default;
+
       const URI = `/api/activity/${activityID}`;
       axios
         .get(URI)
@@ -77,6 +123,10 @@ export default {
       activityHostEmail,
       activityHostNickname,
       activityLastModificationDate,
+      showApplyForm,
+      applyComment,
+      onClickApplyButton,
+      doApply,
     };
   },
 };
@@ -124,5 +174,28 @@ export default {
   flex-direction: column;
   justify-content: space-around;
   min-height: 200px;
+}
+.buttons {
+  position: relative;
+  height: 32px;
+  margin-top: 20px;
+}
+.buttons > .button {
+  overflow: visible;
+  cursor: pointer;
+  min-width: 125px;
+  height: 32px;
+  margin: 0 2px;
+  padding: 0 15px;
+  line-height: 32px;
+  font-size: 14px;
+  border: 1px solid #dfdfdf;
+  background: #fff;
+  border-radius: 10px;
+}
+.buttons > .button.blue {
+  color: #fff;
+  border-color: #0099d2 !important;
+  background: #0099d2 !important;
 }
 </style>
